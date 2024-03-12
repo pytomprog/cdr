c06340c9e4f24da8162f93f8c37a625accceafdf
vporel <dev.vporel@gmail.com>
2024-03-12 15:26:43 +0100

    test commit

--- programs/arm/serialToI2cCommandSender/serialToI2cCommandSender.ino
+++ programs/arm/serialToI2cCommandSender/serialToI2cCommandSender.ino
@@ -1,17 +1,19 @@
 #include <Wire.h>
 
+#define ROLLING_BASE_ADDR 1
+
 void setup() {
   Serial.begin(115200);
   Wire.begin(); // join I2C bus (address optional for master)
   delay(1000); //Wait for slave to power up
-  Wire.beginTransmission(1); // transmit to device 0x01
+  Wire.beginTransmission(ROLLING_BASE_ADDR); // transmit to device
   Wire.write(0b10000000);
   Wire.endTransmission();
 }
 
 void loop() {
   if (Serial.available() > 0) {
-  	Wire.beginTransmission(1); // transmit to device 0x01
+  	Wire.beginTransmission(ROLLING_BASE_ADDR); // transmit to device
     while (Serial.available() > 0) {
       bool immediate = Serial.parseInt();
       byte command = Serial.parseInt();
