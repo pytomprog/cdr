daa724d5ae62a334d6966e3604719afe7747c2ac
pytomprog <62646888+pytomprog@users.noreply.github.com>
2024-10-25 13:26:40 +0200

    Rolling base: New version of the position control mode
    Motors speed is computed with physical equations and not randomly anymore :/
    
    See the previous commit for more details on the brain side code

--- programs/rollingBase/rollingBaseController/rollingBaseController.ino
+++ programs/rollingBase/rollingBaseController/rollingBaseController.ino
@@ -7,19 +7,27 @@
 #define MOTOR2_PWM_PIN 10
 #define MOTOR3_PWM_PIN 11
 
-#define TEST_SPEED 50
+constexpr float SQRT3OVER2 = 0.8660254038f;
+
+constexpr float R = 0.03f;
+constexpr float L = 0.122f;
+
+constexpr float NMaxPhysically = 346.f; // In tr/min, mean of 3 motors
+constexpr float wMaxPhysically = (NMaxPhysically * 2.f * PI) / 60.f; // In rad/s
+constexpr int minPWM = 17;
+constexpr int maxPWM = 255;
+constexpr float k1 = wMaxPhysically;
+constexpr float k2 = (maxPWM - minPWM) / wMaxPhysically;
+constexpr float k3 = minPWM;
 
 char streamChar[32];
 int streamCharPos = 0;
 int incomingByte = 0;
+unsigned long previousReceivedTime = 0;
 
-int motor1Speed = 0;
-int motor2Speed = 0;
-int motor3Speed = 0;
-
-int dX = 0;
-int dY = 0;
-int dTheta = 0;
+int pwm1 = 0;
+int pwm2 = 0;
+int pwm3 = 0;
 
 float motor1TurningCoeff = 1.f;
 float motor2TurningCoeff = 1.f;
@@ -28,22 +36,59 @@ float motor1CorrectionCoeff = 1.f;
 float motor2CorrectionCoeff = 1.f;
 float motor3CorrectionCoeff = 1.f;
 
-unsigned char percentageToPwmValue(unsigned int percentage) {
+inline int clamp(int value, int minValue, int maxValue) { return min(max(value, minValue), maxValue); }
+inline float clamp(float value, float minValue, float maxValue) { return min(max(value, minValue), maxValue); }
+
+int percentageToPwmValue(int percentage) { // Deprecated
   if (percentage == 0) {
     return 0;
   } else {
-    return map(percentage, 1, 100, 15, 255); // so 1% => 15 because under 15, the motors doesn't turn
+    int pwm = map(abs(percentage), 1, 100, minPWM, maxPWM); // so 1% => 15 because under 15, the motors doesn't turn
+    if (percentage < 0) {
+      pwm = -pwm;
+    }
+    return clamp(pwm, -255, 255); 
   }
 }
 
-void i2cReceiverHandler() {
+inline float percentageToW(float percentage) {
+  return k1*percentage; // w = k1*%
+}
+
+inline float wToPWM(float w) {
+  float pwm = 0;
+  if (w != 0) {
+    pwm = k2*abs(w) + k3;
+    if (w < 0)
+      pwm = -pwm;
+  }
+  return clamp(round(pwm), -255, 255);
+}
+
+
+int getIntFromStream() {
   bool neg;
-  motor1Speed = 0;
-  motor2Speed = 0;
-  motor3Speed = 0;
-  dX = 0;
-  dY = 0;
-  dTheta = 0;
+  int value = 0;
+  if (streamChar[streamCharPos] == '-'){
+    neg = true;
+    streamCharPos++;
+  } else {
+    neg = false;
+  }
+  while (isDigit(streamChar[streamCharPos])) {
+    value = value * 10 + streamChar[streamCharPos] - '0';
+    streamCharPos++;
+  }
+  if (neg) {
+    value = -value;
+  }
+  return value;
+}
+
+void i2cReceiverHandler() {
+  pwm1 = 0;
+  pwm2 = 0;
+  pwm3 = 0;
 
   for (streamCharPos = 0; streamCharPos < 32; streamCharPos++){
     streamChar[streamCharPos] = 0;
@@ -56,7 +101,7 @@ void i2cReceiverHandler() {
     streamCharPos++;
 
     if (incomingByte == '\n'){
-      Serial.print(streamChar);
+      //Serial.print(streamChar);
       break;
     }
   }
@@ -64,196 +109,81 @@ void i2cReceiverHandler() {
   streamCharPos = 0;
 
   switch (streamChar[0]) {
-    case 'A':
+    case 'A': {
       while (!isDigit(streamChar[streamCharPos]) && streamChar[streamCharPos]!='-') {
         streamCharPos++;
       }
 
-      // Set motor 1 speed
-      if (streamChar[streamCharPos] == '-'){
-        neg = true;
-        streamCharPos++;
-      } else {
-        neg = false;
-      }
-      while (isDigit(streamChar[streamCharPos])) {
-        motor1Speed = motor1Speed * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
-      }
-      if (neg) {
-        motor1Speed = -motor1Speed;
-      }
+      int motor1Percentage = getIntFromStream();
       streamCharPos++;
-
-      // Set motor 2 speed
-      if (streamChar[streamCharPos] == '-'){
-        neg = true;
-        streamCharPos++;
-      } else {
-        neg = false;
-      }
-      while (isDigit(streamChar[streamCharPos])) {
-        motor2Speed = motor2Speed * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
-      }
-      if (neg) {
-        motor2Speed = -motor2Speed;
-      }
+      int motor2Percentage = getIntFromStream();
       streamCharPos++;
+      int motor3Percentage = getIntFromStream();
 
-      // Set motor 3 speed
-      if (streamChar[streamCharPos] == '-'){
-        neg = true;
-        streamCharPos++;
-      } else {
-        neg = false;
-      }
-      while (isDigit(streamChar[streamCharPos])) {
-        motor3Speed = motor3Speed * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
-      }
-      if (neg) {
-        motor3Speed = -motor3Speed;
-      }
+      motor1Percentage = clamp(motor1Percentage, -100, 100);
+      motor2Percentage = clamp(motor2Percentage, -100, 100);
+      motor3Percentage = clamp(motor3Percentage, -100, 100);
 
-      motor1Speed = min(max(motor1Speed , -100), 100);
-      motor2Speed = min(max(motor2Speed , -100), 100);
-      motor3Speed = min(max(motor3Speed , -100), 100);
+      pwm1 = percentageToPwmValue(motor1Percentage);
+      pwm2 = percentageToPwmValue(motor2Percentage);
+      pwm3 = percentageToPwmValue(motor3Percentage);
 
       break;
-
-    case 'B':
+    }
+    case 'D': {
       while (!isDigit(streamChar[streamCharPos]) && streamChar[streamCharPos]!='-') {
         streamCharPos++;
       }
 
-      // Set dX
-      if (streamChar[streamCharPos] == '-'){
-        neg = true;
-        streamCharPos++;
-      } else {
-        neg = false;
-      }
-      while (isDigit(streamChar[streamCharPos])) {
-        dX = dX * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
-      }
-      if (neg) {
-        dX = -dX;
-      }
-      streamCharPos++;
-
-      // Set dY
-      if (streamChar[streamCharPos] == '-'){
-        neg = true;
-        streamCharPos++;
-      } else {
-        neg = false;
-      }
-      while (isDigit(streamChar[streamCharPos])) {
-        dY = dY * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
-      }
-      if (neg) {
-        dY = -dY;
-      }
-      streamCharPos++;
-
-      // Set dTheta
-      if (streamChar[streamCharPos] == '-'){
-        neg = true;
-        streamCharPos++;
-      } else {
-        neg = false;
-      }
-      while (isDigit(streamChar[streamCharPos])) {
-        dTheta = dTheta * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
-      }
-      if (neg) {
-        dTheta = -dTheta;
-      }
-
-      motor1Speed = int((-dY - motor1TurningCoeff*dTheta) * motor1CorrectionCoeff);
-      motor2Speed = int((0.866*dX + 0.5*dY - motor2TurningCoeff*dTheta) * motor2CorrectionCoeff); // 0.866 = sqrt(3.0)/2.0
-      motor3Speed = int((-0.866*dX + 0.5*dY - motor3TurningCoeff*dTheta) * motor3CorrectionCoeff); // 0.866 = sqrt(3.0)/2.0
-
-      break;
-
-    case 'C':
       while (!isDigit(streamChar[streamCharPos]) && streamChar[streamCharPos]!='-') {
         streamCharPos++;
       }
 
-      // Set dX
-      if (streamChar[streamCharPos] == '-'){
-        neg = true;
-        streamCharPos++;
-      } else {
-        neg = false;
-      }
-      while (isDigit(streamChar[streamCharPos])) {
-        dX = dX * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
-      }
-      if (neg) {
-        dX = -dX;
-      }
+      float vX = getIntFromStream() / 1000.f;
       streamCharPos++;
-
-      // Set dY
-      if (streamChar[streamCharPos] == '-'){
-        neg = true;
-        streamCharPos++;
-      } else {
-        neg = false;
-      }
-      while (isDigit(streamChar[streamCharPos])) {
-        dY = dY * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
-      }
-      if (neg) {
-        dY = -dY;
-      }
+      float vY = getIntFromStream() / 1000.f;
       streamCharPos++;
-
-      // Set dTheta
-      if (streamChar[streamCharPos] == '-'){
-        neg = true;
-        streamCharPos++;
-      } else {
-        neg = false;
-      }
-      while (isDigit(streamChar[streamCharPos])) {
-        dTheta = dTheta * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
-      }
-      if (neg) {
-        dTheta = -dTheta;
-      }
+      float vTheta = getIntFromStream() / 1000.f;
       streamCharPos++;
+      float percentageMax = getIntFromStream() / 100.f;
+      //Serial.println("vX: " + String(vX) + ", vY: " + String(vY) + ", vTheta: " + String(vTheta) + ", %Max: " + String(percentageMax));
 
-      // Set maxPercentage
-      int maxPercentage = 0;
-      while (isDigit(streamChar[streamCharPos])) {
-        maxPercentage = maxPercentage * 10 + streamChar[streamCharPos] - '0';
-        streamCharPos++;
+      float w1 = (-vY - L*vTheta) / R;
+      float w2 = (SQRT3OVER2*vX + 0.5f*vY - L*vTheta) / R;
+      float w3 = (-SQRT3OVER2*vX + 0.5f*vY - L*vTheta) / R;
+      //Serial.println("Before correction: w1: " + String(w1) + ", w2: " + String(w2) + ", w3: " + String(w3));
+
+      float wMax = percentageToW(percentageMax);
+      //Serial.println("wMax: " + String(wMax));
+
+      float w1OverWMax = abs(w1)/wMax;
+      float w2OverWMax = abs(w2)/wMax;
+      float w3OverWMax = abs(w3)/wMax;
+
+      float maxPercentageOfWMax = max(max(w1OverWMax, w2OverWMax), w3OverWMax);
+      //Serial.println("Max percentage of wMax: " + String(maxPercentageOfWMax));
+
+      if (maxPercentageOfWMax >= 1.f) {
+         w1 /= maxPercentageOfWMax;
+         w2 /= maxPercentageOfWMax;
+         w3 /= maxPercentageOfWMax;
       }
-      int maxPwmValue = percentageToPwmValue(maxPercentage);
-      Serial.println(maxPwmValue);
+      //Serial.println("After correction: w1: " + String(w1) + ", w2: " + String(w2) + ", w3: " + String(w3));
 
-      motor1Speed = int((-dY - motor1TurningCoeff*dTheta) * motor1CorrectionCoeff);
-      motor2Speed = int((0.866*dX + 0.5*dY - motor2TurningCoeff*dTheta) * motor2CorrectionCoeff); // 0.866 = sqrt(3.0)/2.0
-      motor3Speed = int((-0.866*dX + 0.5*dY - motor3TurningCoeff*dTheta) * motor3CorrectionCoeff); // 0.866 = sqrt(3.0)/2.0
-
-      float maxPercentageOfMaxPwmValue = max(max((abs(motor1Speed) / float(maxPwmValue)), (abs(motor2Speed) / float(maxPwmValue))), (abs(motor3Speed) / float(maxPwmValue)));
-
-      motor1Speed = int(motor1Speed/maxPercentageOfMaxPwmValue);
-      motor2Speed = int(motor2Speed/maxPercentageOfMaxPwmValue);
-      motor3Speed = int(motor3Speed/maxPercentageOfMaxPwmValue);
+      pwm1 = wToPWM(w1);
+      pwm2 = wToPWM(w2);
+      pwm3 = wToPWM(w3);
+      //Serial.println("Finnaly: pwm1: " + String(pwm1) + ", pwm2: " + String(pwm2) + ", pwm3: " + String(pwm3));
 
       break;
+    }
   }
+
+  while (Wire.available() > 0) {
+    Wire.read();
+  }
+
+  previousReceivedTime = millis();
 }
 
 void setup() {
@@ -278,12 +208,18 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("Motor 1: " + String(motor1Speed) + ", motor 2: " + String(motor2Speed) + ", motor 3: " + String(motor3Speed));
-  digitalWrite(MOTOR1_DIRECTION_PIN, (motor1Speed < 0));
-  digitalWrite(MOTOR2_DIRECTION_PIN, (motor2Speed < 0));
-  digitalWrite(MOTOR3_DIRECTION_PIN, (motor3Speed < 0));
-  analogWrite(MOTOR1_PWM_PIN, percentageToPwmValue(abs(motor1Speed)));
-  analogWrite(MOTOR2_PWM_PIN, percentageToPwmValue(abs(motor2Speed)));
-  analogWrite(MOTOR3_PWM_PIN, percentageToPwmValue(abs(motor3Speed)));
-  delay(100);
+  if (millis() > previousReceivedTime + 1000) {
+    pwm1 = 0;
+    pwm2 = 0;
+    pwm3 = 0;
+  }
+  
+  //Serial.println("Motor 1: " + String(pwm1) + ", motor 2: " + String(pwm2) + ", motor 3: " + String(pwm3));
+  digitalWrite(MOTOR1_DIRECTION_PIN, (pwm1 < 0)); //pwm1
+  digitalWrite(MOTOR2_DIRECTION_PIN, (pwm2 < 0));
+  digitalWrite(MOTOR3_DIRECTION_PIN, (pwm3 < 0));
+  analogWrite(MOTOR1_PWM_PIN, abs(pwm1));
+  analogWrite(MOTOR2_PWM_PIN, abs(pwm2));
+  analogWrite(MOTOR3_PWM_PIN, abs(pwm3));
+  //delay(100);
 }
